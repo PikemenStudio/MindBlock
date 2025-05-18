95c379d8883813e0863ff2902c4dbbe059fd36de
Full-Hat <nikita3131228@gmail.com>
2025-05-18 10:30:40 -0700

    InitUEProject

---
+++ Source/MindBlock/MindBlock.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MindBlock.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MindBlock, "MindBlock" );
+ 
---
+++ Source/MindBlock/MindBlockGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MindBlockGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMindBlockGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMindBlockGameMode();
+};
+
+
+
